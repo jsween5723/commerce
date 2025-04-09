bd1be31dc550120fbf61d08fb5c7624c07f04b76
jsween5723 <jsween5723@gmail.com>
2025-04-10 02:43:57 +0900

    chore(dto): 검증 로직 및 테스트 추가

--- src/main/kotlin/kr/hhplus/be/server/domain/product/ProductCommand.kt
+++ src/main/kotlin/kr/hhplus/be/server/domain/product/ProductCommand.kt
@@ -4,12 +4,13 @@ class ProductCommand {
     data class Release(val targets: List<ProductIdAndQuantity>) {
         init {
             if (targets.isEmpty()) throw ProductException.TargetIsEmpty()
-            targets.forEach { (productId, quantity) ->
+        }
+
+        data class ProductIdAndQuantity(val productId: Long, val quantity: Long) {
+            init {
                 if (productId < 1) throw ProductException.InvalidProductId()
                 if (quantity < 1) throw ProductException.AmountMustGreaterThanZero()
             }
         }
-
-        data class ProductIdAndQuantity(val productId: Long, val quantity: Long)
     }
 }
