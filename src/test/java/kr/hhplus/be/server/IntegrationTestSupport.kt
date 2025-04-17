102d9cdb8c6b80fa8c981bcb1017e60d33b7702c
jsween5723 <jsween5723@gmail.com>
2025-04-17 23:51:26 +0900

    test(product): product 통합테스트 추가

--- src/test/java/kr/hhplus/be/server/IntegrationTestSupport.kt
+++ src/test/java/kr/hhplus/be/server/IntegrationTestSupport.kt
@@ -9,8 +9,6 @@ import kr.hhplus.be.server.domain.product.RankedProduct
 import org.hibernate.SessionFactory
 import org.instancio.Instancio
 import org.instancio.Select.field
-import org.junit.jupiter.api.BeforeAll
-import org.junit.jupiter.api.TestInstance
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.context.SpringBootTest
 import org.springframework.data.jpa.repository.JpaRepository
@@ -20,8 +18,15 @@ import java.time.temporal.ChronoUnit
 import kotlin.random.Random
 
 @SpringBootTest
-@TestInstance(TestInstance.Lifecycle.PER_CLASS)
 class IntegrationTestSupport {
+    private lateinit var rankedProducts: List<RankedProduct>
+    private lateinit var coupons: List<Coupon>
+
+    companion object {
+        const val MAX_COUNT = 10L
+        const val MAX_STOCK_NUMBER = 1000L
+    }
+
     @Autowired
     lateinit var couponRepository: JpaRepository<Coupon, Long>
 
@@ -39,52 +44,64 @@ class IntegrationTestSupport {
 
     @Autowired
     lateinit var pointRepository: JpaRepository<UserPoint, Long>
+    protected val idGenerator = LongFixture()
+    protected lateinit var products: List<Product>
 
     fun insertTemplate(entities: Collection<Any>) {
         val it =
             entityManagerFactory.unwrap(SessionFactory::class.java).withStatelessOptions().openStatelessSession()
         val tx = it.beginTransaction()
         try {
-            entities.forEach { entity -> it.insert(entity) }
+            entities.map { entity -> it.insert(entity) }
+            tx.commit()
         } catch (e: Exception) {
             tx.rollback()
         } finally {
-            tx.commit()
+            it.close()
         }
     }
 
-    @BeforeAll
-    fun beforeEach() {
-        insertTemplate(IntRange(0, 100).map {
-            Instancio.of(Coupon::class.java).supply(field("discountAmount")) { _ ->
-                val number = Random.nextInt(100)
-                BigDecimal(number)
-            }.generate(field("expireDuration")) { gen ->
-                gen.temporal().duration().min(100, ChronoUnit.DAYS)
-            }.generate(field("publishFrom")) { gen -> gen.temporal().localDateTime().past() }
-                .generate(field("publishTo")) { gen -> gen.temporal().localDateTime().future() }
-                .ignore(field("id"))
-                .create()
-        })
+    fun insertProducts() {
+        products = LongRange(0, MAX_COUNT).map {
+            Instancio.of(Product::class.java)
+                .generate(field("stockNumber")) { gen -> gen.longs().min(500).max(1000) }
+                .supply(
+                    field("price")
+                ) { _ -> BigDecimal.valueOf(Random.nextInt(300, 3000).toLong()) }
+                .ignore(field("id")).create()
+        }
         insertTemplate(
-            IntRange(0, 100).map {
-                Instancio.of(Product::class.java)
-                    .generate(field("stockNumber")) { gen -> gen.longs().min(500) }
-                    .supply(
-                        field("price")
-                    ) { _ -> BigDecimal.valueOf(Random.nextInt(300, 3000).toLong()) }
-                    .ignore(field("id")).create()
-            }
+            products
         )
+    }
+
+    fun insertRankedProducts() {
         val longFixture = LongFixture()
-        insertTemplate(IntRange(0, 100).map {
+        rankedProducts = LongRange(0, MAX_COUNT).map {
             Instancio.of(RankedProduct::class.java).set(field("productId"), longFixture.productId())
                 .ignore(field("id"))
                 .generate(field("totalSellingCount")) { gen -> gen.longs().min(1).max(10000) }
                 .supply(field("totalIncome")) { gen -> BigDecimal(Random.nextInt(0, 10000)) }
                 .supply(field("createdDate")) { gen -> LocalDate.now() }
                 .create()
-        })
+        }
+        insertTemplate(rankedProducts)
+    }
+
+    fun insertCoupons() {
+        coupons = LongRange(0, MAX_COUNT).map {
+            Instancio.of(Coupon::class.java).supply(field("discountAmount")) { _ ->
+                val number = Random.nextLong(MAX_COUNT)
+                BigDecimal(number)
+            }.generate(field("expireDuration")) { gen ->
+                gen.temporal().duration().min(MAX_COUNT, ChronoUnit.DAYS)
+            }.generate(field("publishFrom")) { gen -> gen.temporal().localDateTime().past() }
+                .generate(field("publishTo")) { gen -> gen.temporal().localDateTime().future() }
+                .ignore(field("id"))
+                .create()
+
+        }
+        insertTemplate(coupons)
     }
 }
 
--- src/test/java/kr/hhplus/be/server/domain/product/ProductServiceIntegrationTest.kt
+++ src/test/java/kr/hhplus/be/server/domain/product/ProductServiceIntegrationTest.kt
@@ -1,34 +1,151 @@
 package kr.hhplus.be.server.domain.product
 
+import kr.hhplus.be.server.IntegrationTestSupport
+import org.assertj.core.api.Assertions.assertThat
+import org.assertj.core.api.Assertions.assertThatThrownBy
+import org.instancio.Instancio
+import org.instancio.Select.field
+import org.junit.jupiter.api.BeforeAll
 import org.junit.jupiter.api.Nested
 import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.TestInstance
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.data.repository.findByIdOrNull
 
-class ProductServiceIntegrationTest {
-//    private val productRepository = mock(ProductRepository::class.java)
-//    private val productService = ProductService(productRepository)
+@SpringBootTest
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
+class ProductServiceIntegrationTest : IntegrationTestSupport() {
+    @Autowired
+    lateinit var productService: ProductService
+
+    @BeforeAll
+    fun beforeAll() {
+        insertProducts()
+    }
 
     @Nested
     inner class `상품을 출고할 수 있다` {
 
         @Test
         fun `상품을 출고 시키면 Product_Release_VO를 반환한다`() {
-//            TODO: ID를 기반으로 mapping을 수행하기에 영속계층과 연계 해야하며 단위기능 자체는 Product 테스트로 충족가능
-            //given
-//            val releaseAmount = 2L
-//            val command = ProductCommand.Release(
-//                listOf(
-//                    ProductCommandFixture.ProductIdAndQuantity.create(
-//                        productId = 1L, quantity = releaseAmount
-//                    ),
-//                )
-//            )
-//            val targetIds = command.targets.map { it.productId }
-//            val product = ProductFixture(id = 1L, stockNumber = 10)
-//            val expected = listOf(product.ReleaseVO(quantity = releaseAmount))
-//            `when`(productRepository.findByIds(targetIds)).thenReturn(listOf(product))
-//            //when
-//            val released = productService.release(command)
-//            assertThat(released).isEqualTo(expected)
+            val releaseAmount = 2L
+            val productId = idGenerator.productId()
+            val command = ProductCommand.Release(
+                listOf(
+                    ProductCommandFixture.ProductIdAndQuantity.create(
+                        productId = productId, quantity = releaseAmount
+                    ),
+                )
+            )
+            val targetIds = command.targets.map { it.productId }
+            val expected = productRepository.findByIdOrNull(productId)!!.release(releaseAmount)
+            //when
+            val released = productService.release(command)
+            assertThat(released[0]).extracting({ it.quantity }, { it.product.id })
+                .contains(expected.quantity, expected.product.id)
+        }
+
+        @Test
+        fun `상품을 출고시키면 값이 감소한다`() {
+            val targets = IntRange(1, MAX_COUNT.toInt()).map {
+                Instancio.of(ProductCommand.ProductIdAndQuantity::class.java)
+                    .supply(field("productId")) { gen ->
+                        it
+                    }
+                    .generate(field("quantity")) { gen ->
+                        gen.longs().min(1).max(10)
+                    }
+                    .create()
+            }.toList()
+            val command = ProductCommand.Release(targets)
+            val set = targets.map { it.productId }.toSet()
+            val expected = productRepository.findAll().filter { set.contains(it.id) }
+                .mapIndexed { index, it -> it.stockNumber - targets[index].quantity }.toSet()
+            //when
+            productService.release(command)
+            val result = productRepository.findAll().filter { set.contains(it.id) }.map { it.stockNumber }.toSet()
+            assertThat(result).containsAll(expected)
+        }
+
+        @Test
+        fun `존재하지 않는 상품이라면 ProductException을 발생시킨다`() {
+            val targets = IntRange(1, MAX_COUNT.toInt()).map {
+                Instancio.of(ProductCommand.ProductIdAndQuantity::class.java)
+                    .supply(field("productId")) { gen ->
+                        it
+                    }
+                    .generate(field("quantity")) { gen ->
+                        gen.longs().min(1).max(10)
+                    }
+                    .create()
+            }.toMutableList()
+            targets.add(ProductCommand.ProductIdAndQuantity(MAX_COUNT.plus(2), 10))
+            val command = ProductCommand.Release(targets)
+            //when
+            assertThatThrownBy { productService.release(command) }.isInstanceOf(ProductException::class.java)
+        }
+
+        @Test
+        fun `수량이 부족하면 ProductException을 발생시킨다`() {
+            val targets = IntRange(1, MAX_COUNT.toInt()).map {
+                Instancio.of(ProductCommand.ProductIdAndQuantity::class.java)
+                    .supply(field("productId")) { gen ->
+                        it
+                    }
+                    .generate(field("quantity")) { gen ->
+                        gen.longs().min(1).max(10)
+                    }
+                    .create()
+            }.toMutableList()
+            targets.add(ProductCommand.ProductIdAndQuantity(MAX_COUNT.plus(2), MAX_STOCK_NUMBER + 2))
+            val command = ProductCommand.Release(targets)
+            //when
+            assertThatThrownBy { productService.release(command) }.isInstanceOf(ProductException::class.java)
+        }
+    }
+
+    @Nested
+    inner class `상품을 적재할 수 있다` {
+
+        @Test
+        fun `상품을 적재 시키고 조회하면 값이 증가한다`() {
+            val targets = IntRange(1, MAX_COUNT.toInt()).map {
+                Instancio.of(ProductCommand.ProductIdAndQuantity::class.java)
+                    .supply(field("productId")) { gen ->
+                        it
+                    }
+                    .generate(field("quantity")) { gen ->
+                        gen.longs().min(1).max(10)
+                    }
+                    .create()
+            }.toList()
+            val command = ProductCommand.Restock(targets)
+            val set = targets.map { it.productId }.toSet()
+            val expected = productRepository.findAll().filter { set.contains(it.id) }
+                .mapIndexed { index, it -> it.stockNumber + targets[index].quantity }.toSet()
+            //when
+            productService.restock(command)
+            val result = productRepository.findAll().filter { set.contains(it.id) }.map { it.stockNumber }.toSet()
+            assertThat(result).containsAll(expected)
+        }
+
+        @Test
+        fun `일부가 존재하지 않는 상품이라면 ProductException을 발생시킨다`() {
+            val targets = IntRange(1, MAX_COUNT.toInt()).map {
+                Instancio.of(ProductCommand.ProductIdAndQuantity::class.java)
+                    .supply(field("productId")) { gen ->
+                        it
+                    }
+                    .generate(field("quantity")) { gen ->
+                        gen.longs().min(1).max(10)
+                    }
+                    .create()
+            }.toMutableList()
+            targets.add(ProductCommand.ProductIdAndQuantity(MAX_COUNT.plus(2), 10))
+            val command = ProductCommand.Restock(targets)
+            //when
+            assertThatThrownBy { productService.restock(command) }.isInstanceOf(ProductException::class.java)
         }
     }
 }
