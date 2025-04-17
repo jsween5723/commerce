766f7b615eed4808216bf667661ee329f205e1c2
jsween5723 <jsween5723@gmail.com>
2025-04-18 01:47:52 +0900

    test(coupon): couponService 통합테스트 작성

--- src/main/kotlin/kr/hhplus/be/server/domain/coupon/Coupon.kt
+++ src/main/kotlin/kr/hhplus/be/server/domain/coupon/Coupon.kt
@@ -35,7 +35,7 @@ class Coupon(
     @UpdateTimestamp
     lateinit var updatedAt: LocalDateTime
 
-    @OneToMany(mappedBy = "coupon", cascade = [CascadeType.MERGE, CascadeType.REMOVE], orphanRemoval = true)
+    @OneToMany(mappedBy = "coupon", cascade = [CascadeType.ALL], orphanRemoval = true)
     val publishedCoupons: MutableList<PublishedCoupon> = mutableListOf()
 
     fun publish(userId: UserId, now: LocalDateTime): PublishedCoupon {
