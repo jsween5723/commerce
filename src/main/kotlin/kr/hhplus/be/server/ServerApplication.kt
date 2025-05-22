a5b753e0cfa2d657a95672ef0e1cde03c0043bd4
jsween5723 <jsween5723@gmail.com>
2025-05-22 18:08:45 +0900

    fix: 비동기 적용

--- src/main/kotlin/kr/hhplus/be/server/ServerApplication.kt
+++ src/main/kotlin/kr/hhplus/be/server/ServerApplication.kt
@@ -3,11 +3,13 @@ package kr.hhplus.be.server
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
 import org.springframework.retry.annotation.EnableRetry
+import org.springframework.scheduling.annotation.EnableAsync
 import org.springframework.scheduling.annotation.EnableScheduling
 
 @SpringBootApplication
 @EnableScheduling
 @EnableRetry
+@EnableAsync
 class ServerApplication
 
 fun main(args: Array<String>) {
